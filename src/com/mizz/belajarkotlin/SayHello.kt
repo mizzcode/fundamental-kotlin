3db7f4a20e2454b15fe71a233c46ddce27e77fcf
Misbahudin <misbahudin.id@gmail.com>
2024-03-28 20:59:14 +0700

    Initial commit

---
+++ src/com/mizz/belajarkotlin/SayHello.kt
@@ -0,0 +1,13 @@
+package com.mizz.belajarkotlin
+
+class SayHello {
+    fun halo(nama: String) {
+        print("Halo $nama")
+    }
+}
+
+fun main() {
+val indo = SayHello();
+
+    indo.halo("Mizz")
+}
